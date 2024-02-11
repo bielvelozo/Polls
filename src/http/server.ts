4919f1dbb87ce18066fa4a2f4050c19a2b353435
bielvelozo <gabrielvelozodev@gmail.com>
2024-02-10 21:42:40 -0300

    chore: folders organization

---
+++ src/http/server.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+import fastify from "fastify";
+import z from "zod";
+import { createPoll } from "./routes/create-poll";
+
+const app = fastify();
+
+app.register(createPoll);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP server running! 🚀");
+});
